d4469b53417d4721990a3acac1ee2222743e8592
AreebAhmed812 <ahmedareeb506@gmail.com>
2022-08-26 00:31:18 +0500

    First Message

---
+++ resources/views/user/doctor.blade.php
@@ -0,0 +1,39 @@
+<link rel="stylesheet" href="../assets/css/maicons.css">
+
+  <link rel="stylesheet" href="../assets/css/bootstrap.css">
+
+  <link rel="stylesheet" href="../assets/vendor/owl-carousel/css/owl.carousel.css">
+
+  <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+
+  <link rel="stylesheet" href="../assets/css/theme.css">
+
+<div class="page-section">
+    <div class="container">
+      <h1 class="text-center mb-5 wow fadeInUp">Our Doctors</h1>
+
+      <div class="owl-carousel wow fadeInUp" id="doctorSlideshow">
+
+        @foreach($doctor as $doctors)
+        <div class="item">
+          <div class="card-doctor">
+            <div class="header">
+              <img style="height: 300px !important" src="doctorimage/{{$doctors->image}}" alt="">
+              <div class="meta">
+                <a href="#"><span class="mai-call"></span></a>
+                <a href="#"><span class="mai-logo-whatsapp"></span></a>
+              </div>
+            </div>
+            <div class="body">
+              <p class="text-xl mb-0">{{$doctors->name}}</p>
+              <span class="text-sm text-grey">{{$doctors->speciality}}</span>
+            </div>
+          </div>
+        </div>
+
+        @endforeach
+        
+       
+      </div>
+    </div>
+  </div>
